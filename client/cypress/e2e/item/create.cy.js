fcb7bdc7828802c1c0be8f0d98ac87c18e529b03
Dhanu297 <dhanashree.thakur@gmail.com>
2026-03-25 13:00:04 -0600

    test case for create item

---
+++ client/cypress/e2e/item/create.cy.js
@@ -0,0 +1,43 @@
+describe('Create Item Flow - NeverLose', () => {
+
+  beforeEach(() => {
+    cy.visit('/dashboard'); // assuming user already logged in
+  });
+
+  it('should create a new item successfully', () => {
+
+    // Click Add New Item
+    cy.contains('Add New Item').click();
+
+    // Verify navigation
+    cy.url().should('include', '/create-item');
+
+    // Step 1: Fill form
+   cy.contains('Item Nickname')
+  .parent()
+  .find('input')
+  .type('Test Item Cypress Backpack');
+    cy.get('textarea').type('This is a test item created by Cypress- backpack');
+
+    // Upload image (mock file)
+    cy.get('input[type="file"]').selectFile('cypress/fixtures/backpack.jpeg', { force: true });
+
+    // Click Next
+    cy.contains('Next').click();
+
+    // Step 2 (Assumption - update if needed)
+    cy.contains('Review & Create').click();
+
+    // Step 3 - Submit
+    cy.contains('Create Item').click();
+
+    // Assertion
+    //cy.contains('Item created successfully').should('be.visible');
+
+    // Redirect to dashboard
+    cy.url().should('include', '/');
+
+    // Verify item exists
+    cy.contains('Test Item Cypress').should('be.visible');
+  });
+});
